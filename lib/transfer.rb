9017ac3fbbb0bb459beef47da490dca3af9fec9a
Conor Owens <conorj.owens@gmail.com>
2019-05-25 17:50:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid?
+    if @sender.balance
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
